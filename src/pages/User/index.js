03c184a71f95a9ee370357d795c112c44b8d4c5e
rurbano3d <rurbano3d@gmail.com>
2019-09-25 08:07:42 -0300

    chore:Add loading on starred repos

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -14,6 +14,7 @@ import {
   Info,
   Title,
   Author,
+  Loading,
 } from './styles';
 
 export default class User extends Component {
@@ -29,19 +30,24 @@ export default class User extends Component {
 
   state = {
     stars: [],
+    loading: false,
   };
 
   async componentDidMount() {
     const {navigation} = this.props;
+    const {loading} = this.state;
     const user = navigation.getParam('user');
-
+    this.setState({loading: true});
     const response = await api.get(`/users/${user.login}/starred`);
-    this.setState({stars: response.data});
+    this.setState({
+      stars: response.data,
+      loading: false,
+    });
   }
 
   render() {
     const {navigation} = this.props;
-    const {stars} = this.state;
+    const {stars, loading} = this.state;
     const user = navigation.getParam('user');
     return (
       <Container>
@@ -50,20 +56,23 @@ export default class User extends Component {
           <Name>{user.name}</Name>
           <Bio>{user.bio}</Bio>
         </Header>
-
-        <Stars
-          data={stars}
-          keyExtractor={star => star.id}
-          renderItem={({item}) => (
-            <Starred>
-              <OwnerAvatar source={{uri: item.owner.avatar_url}} />
-              <Info>
-                <Title>{item.name}</Title>
-                <Author>{item.owner.login}</Author>
-              </Info>
-            </Starred>
-          )}
-        />
+        {loading ? (
+          <Loading />
+        ) : (
+          <Stars
+            data={stars}
+            keyExtractor={star => star.id}
+            renderItem={({item}) => (
+              <Starred>
+                <OwnerAvatar source={{uri: item.owner.avatar_url}} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
       </Container>
     );
   }
